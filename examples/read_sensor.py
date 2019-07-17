78746233f395321df2c7ad1073a7f82bd8300032
Juan Gallostra <juangallostra@gmail.com>
2019-07-17 23:57:39 +0200

    Comment code

--- examples/read_sensor.py
+++ examples/read_sensor.py
@@ -14,35 +14,50 @@ from cflib.crtp.crtpstack import CRTPPort
 
 
 def scan_for_crazyflies():
+    """
+    Scan for available crazyflies
+    """
     cflib.crtp.init_drivers()
     available = cflib.crtp.scan_interfaces()
     return available
 
-
 def incoming(packet):
     """
-    Callback for data received from the copter.
+    Callback for data received from the copter. This gets
+    called when a message from SENSOR port is received
     """
     # This might be done prettier ;-)
     print("Message received:")
     print("Header: " + str(packet.header))
+    # We know that the current answer is a float so
+    # this unpack shouldn't fail.
     print("Data: " + str(struct.unpack('<f', packet.data)[0]))
     print("\n")
 
-if __name__ == "__main__":
-    cfies = scan_for_crazyflies()
+def main():
+    found_cfies = scan_for_crazyflies()
+    # Create Crazyflie instance
     cf = cflib.crazyflie.Crazyflie(rw_cache='./cache')
     cf.add_port_callback(CRTPPort.SENSOR, incoming)
-    cf.open_link(cfies[0][0])
+    # Connect to the first detected crazyflie
+    # found_cfies[0][0] contains the uri of the drone
+    cf.open_link(found_cfies[0][0])
     print("Link opened\n")
-    # Create sensor packet
+    # Create packet aimed at the SENSOR port
     pk = CRTPPacket()
     pk.port = CRTPPort.SENSOR
+    # Fill it with dummy data. Eventually this should
+    # be changed by the ID of the sensor
     pk.data = struct.pack('<B', 1)
     print("Sending message:")
     print("Header: " + str(pk.header))
     print("Data: " + str(pk.data))
     print("\n")
     cf.send_packet(pk)
-    time.sleep(1)
+    time.sleep(1) # Wait to receive answer
+    # Incoming messages are received and processed in a different
+    # thread. See the _IncomingPacketHandler class at cflib/crazyflie/__init__.py 
     cf.close_link()
+
+if __name__ == "__main__":
+    main()
